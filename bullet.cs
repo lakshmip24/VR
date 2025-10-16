1ee236a38e467fb7b8cd3541e6c737752606567d
lakshmip24 <lakshmipradeep2411@gmail.com>
2025-10-16 06:03:32 +0530

    Updated bullet script and fixed collision logic

--- bullet.cs
+++ bullet.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class bullet : MonoBehaviour
 {
+    int score;
+    int turn;
+    Rigidbody rb;
+    public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
+        UpdateScoreText();
+        rb = GetComponent<Rigidbody>();
+        turn=0;
+        score=0;
         
     }
 
@@ -15,32 +24,59 @@ public class bullet : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(-1.0f, 0.0f, 0.0f, Space.Self);
+            transform.Translate(0.0f, 0.0f, -0.1f);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(1.0f, 0.0f, 0.0f, Space.Self);
+            transform.Translate(0.0f, 0.0f, 0.1f);
         }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Rotate(0.0f, 1.0f, 0.0f, Space.Self);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Rotate(0.0f, -1.0f, 0.0f, Space.Self);
-        }
+        
         if (Input.GetKey(KeyCode.Space))
         {
-            transform.Translate(0.0f, 0.7f, 0.0f, Space.Self);
+
+            
+            rb.isKinematic = false;
+             rb.velocity = transform.right * 5;
+             rb.useGravity = false;
+             rb.freezeRotation = true;
+             turn=turn+1;
+             Debug.Log("Turn: " + turn);
         }
     }
-    onCollisionEnter(Collision collision)
+   void OnCollisionEnter(Collision collision)
+{
+    
+    if (collision.gameObject.CompareTag("sphere"))
     {
-        if (collision.gameObject.tag == "balloon")
+        Destroy(collision.gameObject);
+        score += 1;
+        Debug.Log("Score: " + score);
+        UpdateScoreText();
+        if(turn<4)
         {
-            Destroy(gameObject);
-            score.score+=1;
+            rb.isKinematic = true;
+            rb.position = new Vector3(-11.12f, 1.295f, 0.0f);
+            Debug.Log("hi ");
         }
+        if(rb.position.x==0)
+        {
+            rb.isKinematic = true;
+            rb.position = new Vector3(-11.12f, 1.295f, 0.0f);
+        }
+        if (score == 3)
+        {
+            // Load scene named "2"
+            UnityEngine.SceneManagement.SceneManager.LoadScene("2");
+        }
+        
+    }
+    
+
+}
+void UpdateScoreText()
+    {
+        if(scoreText != null)
+        scoreText.text = "Score: " + score;
     }
 
 }
